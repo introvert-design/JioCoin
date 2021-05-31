eb17cb5ea33c93b1a50946bf75ecb2e76b0caa98
GR <geo.rosy+git@protonmail.ch>
2021-05-31 15:03:10 +0530

    3.Flask & MySQL setup

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+from config import _mysql_user, _mysql_password
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = _mysql_user
+app.config['MYSQL_PASSWORD'] = _mysql_password
+app.config['MYSQL_DB'] = 'jiocoin'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+
+@app.route('/')
+def home():
+    return 'Testing...!'
+
+
+if __name__ == '__main__':
+    app.secret_key = 'xyz123'
+    app.run(host='localhost', port=8000, debug=True)
